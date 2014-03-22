ed897e2f8c69505d63546141070832cfa8411467
nc <nc@waterloo.home>
2014-03-22 14:17:08 -0700

    Characters and Terrain points are now Python classes

--- db_init.py
+++ db_init.py
@@ -1,74 +1,45 @@
-import MySQLdb
+import json
 
-#Connect to localhost - user must be created first
-db = MySQLdb.connect("localhost", "banada", "banada")
-#Create a separate workspace (cursor instance) with the connection
-cursor = db.cursor()
+#Players and Enemies
+class character:
+    def __init__(self, name, x, y, speed, is_player=0):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.is_player = is_player
+        print("Character "+str(self.name)+" created: (" \
+               +str(self.x)+","+str(self.y)+"), "+str(self.speed)+" speed")
+    
+    #Change character position/stats - all args optional
+    def update(self, x_new=-1, y_new=-1, speed_new=-1):
+        if x_new >= 0:
+            self.x = x_new
+        if y_new >= 0:
+            self.y = y_new
+        if speed_new >= 0:
+            self.speed = speed_new
+        print("Updated character "+str(self.name)+": (" \
+              +str(self.x)+","+str(self.y)+"), "+str(self.speed)+" speed")
 
-def create_db():
-    cursor.execute("CREATE DATABASE sunview_rpg")
-    cursor.execute("use sunview_rpg")
-    cursor.execute("CREATE TABLE map (\
-                     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,\
-                     x INT,\
-                     y INT,\
-                     terrain VARCHAR(32)\
-                   )")
-    cursor.execute("CREATE TABLE characters (\
-                     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,\
-                     name VARCHAR(32),\
-                     x INT,\
-                     y INT,\
-                     speed INT,\
-                     is_player BIT(1) NOT NULL DEFAULT 0\
-                   )")
+#Points on the grid
+class terrain:
+    def __init__(self, x, y, ttype):
+        self.x = x
+        self.y = y
+        self.ttype = ttype
+        print("Terrain point created at: (" \
+               +str(self.x)+","+str(self.y)+"), type: "+str(self.ttype))
+    
+    def update(self, ttype_new):
+        self.ttype = ttype_new
+        print("Terrain point (" \
+               +str(self.x)+","+str(self.y)+"), is now type: "+str(self.ttype))
+    
 
-#Set map dimensions
-def set_map_dim(x_dim, y_dim):
-    for x in range(0,x_dim):
-        for y in range (0,y_dim):
-            cursor.execute("INSERT INTO map(x,y) VALUES(%s,%s)", (x, y))
-            db.commit()
+#Testing
+ptf = character("Perezoso the Folivore", 0, 0, 1)
+ptf.update(1,1,0)
 
-#Create player
-def create_player(player_name, x_dim, y_dim, speed):
-    cursor.execute("INSERT INTO characters(name,x,y,speed)\
-                    VALUES(%s,%s,%s,%s)", (player_name, x_dim, y_dim, speed))
-    db.commit()
-
-#Move a player / Change their stats
-def set_player(player_name, x_dim=-1, y_dim=-1, speed=-1):
-
-    #Request and store previous values - this allows optional args
-    cursor.execute("SELECT speed FROM characters WHERE name=%s", (player_name))
-    prev_speed = cursor.fetchone()
-    if speed < 0:
-        speed = prev_speed
-    print(speed)
-
-    cursor.execute("SELECT x FROM characters WHERE name=%s", (player_name))
-    prev_x = cursor.fetchone()
-    if x_dim < 0:
-        x_dim = prev_x
-    print(x_dim)
-
-    cursor.execute("SELECT y FROM characters WHERE name=%s", (player_name))
-    prev_y = cursor.fetchone()
-    if y_dim < 0:
-        y_dim = prev_y
-    print(y_dim)
-
-    cursor.execute("UPDATE characters SET\
-                     x=%s,\
-                     y=%s,\
-                     speed=%s\
-                    WHERE name=%s", (x_dim, y_dim, speed, player_name))
-    db.commit()
-
-#Set/Destroy a wall
-
-#TESTING
-create_db()
-set_map_dim(5, 5)
-create_player("Perezoso the Folivore",0,0,1)
-set_player("Perezoso the Folivore",1,1,0)
+t1 = terrain(0,0,"wall")
+t1.update("door")
--- db_init_2.py
+++ db_init_2.py
@@ -1,5 +1,6 @@
 import json
 
+#Players and Enemies
 class character:
     def __init__(self, name, x, y, speed, is_player=0):
         self.name = name
@@ -9,15 +10,36 @@ class character:
         self.is_player = is_player
         print("Character "+str(self.name)+" created: (" \
                +str(self.x)+","+str(self.y)+"), "+str(self.speed)+" speed")
+    
     #Change character position/stats - all args optional
-    def set_character(self, x_new=-1, y_new=-1, speed_new=-1):
-        if x_new > 0:
+    def update(self, x_new=-1, y_new=-1, speed_new=-1):
+        if x_new >= 0:
             self.x = x_new
-        if y_new > 0:
+        if y_new >= 0:
             self.y = y_new
-        if speed_new > 0:
+        if speed_new >= 0:
             self.speed = speed_new
         print("Updated character "+str(self.name)+": (" \
               +str(self.x)+","+str(self.y)+"), "+str(self.speed)+" speed")
+
+#Points on the grid
+class terrain:
+    def __init__(self, x, y, ttype):
+        self.x = x
+        self.y = y
+        self.ttype = ttype
+        print("Terrain point created at: (" \
+               +str(self.x)+","+str(self.y)+"), type: "+str(self.ttype))
+    
+    def update(self, ttype_new):
+        self.ttype = ttype_new
+        print("Terrain point (" \
+               +str(self.x)+","+str(self.y)+"), is now type: "+str(self.ttype))
+    
+
+#Testing
 ptf = character("Perezoso the Folivore", 0, 0, 1)
-ptf.set_character(1,1,0)
+ptf.update(1,1,0)
+
+t1 = terrain(0,0,"wall")
+t1.update("door")
