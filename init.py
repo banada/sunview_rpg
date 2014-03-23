0f74963a2ccba07fbd6d6d1b196da0aae44c49e5
Nathaniel Chen <nathanielchen1@gmail.com>
2014-03-22 22:56:32 -0400

    Added GET

--- init.py
+++ init.py
@@ -1,21 +1,29 @@
 import json
+import requests
+
+# -Character class
+# -Terrain (map point) class
+# -Map (2-D list of Terrain objects)
+# -GET/POST methods are in the character/terrain classes
 
 #Players and Enemies
 class character:
-    def __init__(self, name, ctype, x, y, speed, is_player=0, is_current=0):
+    def __init__(self, name, ctype, x, y, speed=30, is_player=0, is_current=0, initiative=0):
         self.name = name
+        self.safe_name = name.replace(" ", "_")
         self.ctype = ctype
         self.x = x
         self.y = y
         self.speed = speed
         self.is_player = is_player
         self.is_current = is_current
+        self.initiative = initiative
         print(str(self.ctype)+" character "+str(self.name)+" created: ("\
              +str(self.x)+","+str(self.y)+"), "+str(self.speed)+" speed")
         self.json()
  
     #Change character position/stats - all args optional
-    def update(self, x_new=-1, y_new=-1, speed_new=-1, is_current_new=-1):
+    def update(self, x_new=-1, y_new=-1, speed_new=-1, is_current_new=-1, initiative_new=-1):
         if x_new >= 0:
             self.x = x_new
         if y_new >= 0:
@@ -24,15 +32,15 @@ class character:
             self.speed = speed_new
         if is_current_new >= 0:
             self.is_current = is_current_new
+        if initiative_new >= 0:
+            self.initiative = initiative_new
         print("Updated character "+str(self.name)+": (" \
               +str(self.x)+","+str(self.y)+"), "+str(self.speed)+" speed")
         self.json()
 
     #Writes current self.* to disk as JSON
     def json(self):
-        unsafe_name = self.name
-        safe_name = unsafe_name.replace(" ", "_")
-        with open("./JSON/"+str(safe_name)+".json", "w")\
+        with open("./JSON/"+str(self.safe_name)+".json", "w")\
         as outfile:
             json.dump({'name':str(self.name),
                        'ctype':str(self.ctype),
@@ -40,7 +48,24 @@ class character:
                        'y':str(self.y),
                        'speed':str(self.speed),
                        'is_player':str(self.is_player),
-                       'is_current':str(self.is_current)}, outfile)
+                       'is_current':str(self.is_current),
+                       'initiative':str(self.initiative)}, outfile)
+
+    def get(self):
+        request = requests.get('http://localhost:8000/JSON/'+self.safe_name)
+        j = json.loads(request.text)
+        self.update(str(j['x']), str(j['y']), str(j['speed']), str(j['is_current']), str(j['initiative']))
+
+    def post(self):
+        payload = json.dumps({'name':str(self.name),
+                       'ctype':str(self.ctype),
+                       'x':str(self.x), 
+                       'y':str(self.y),
+                       'speed':str(self.speed),
+                       'is_player':str(self.is_player),
+                       'is_current':str(self.is_current),
+                       'initiative':str(self.initiative)})
+        request = requests.get('http://localhost:8000/JSON/'+self.safe_name, payload)
 
 #Points on the grid
 class terrain:
